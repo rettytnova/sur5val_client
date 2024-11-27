38ace56e9b5d2b5034a5e5a4797e73854d9929b8
성원 정 <tyhg56@naver.com>
2024-11-27 15:56:10 +0900

    fix : " BindingKey 수정 "
    BindingKey
     - en_QuickSlot 수정
      - QUICK_SLOT_MOVE_UP ( 위로 움직이기 )
      - QUICK_SLOT_MOVE_DOWN ( 아래로 움직이기 )
      - QUICK_SLOT_MOVE_LEFT ( 왼쪽으로 움직이기 )
      - QUICK_SLOT_MOVE_RIGHT ( 오른쪽으로 움직이기 )
    
     - en_KeyCode 수정
      - W, S, A, D keycode 추가

--- Assets/_Project/Scripts/Common/BindingKey.cs
+++ Assets/_Project/Scripts/Common/BindingKey.cs
@@ -4,6 +4,11 @@ public enum en_QuickSlot
 
     QUICK_SLOT_UI_INVENTORY,
 
+    QUICK_SLOT_MOVE_UP,
+    QUICK_SLOT_MOVE_DOWN,
+    QUICK_SLOT_MOVE_LEFT,
+    QUICK_SLOT_MOVE_RIGHT,    
+
     QUICK_SLOT_SKILL_USE
 }
 
@@ -13,7 +18,12 @@ public enum en_KeyCode
 
     KEY_CODE_I,
 
-    KEY_CODE_SPACE
+    KEY_CODE_SPACE,
+
+    KEY_CODE_W,
+    KEY_CODE_S,
+    KEY_CODE_A,
+    KEY_CODE_D
 }
 
 public class BindingKey
