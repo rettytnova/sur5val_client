d780bddc01cd9fcd09adab8fd422900bd194636c
성원 정 <tyhg56@naver.com>
2024-12-03 11:30:07 +0900

    add : " UIGameScene 추가 "
    UIGameScene
     - UIChattingInput 추가 ( 채팅 입력 창 )

---
+++ Assets/Scripts/UI/Scene/UIGameScene.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UIGameScene : UIBaseTwo
+{
+    public UIChattingInput uiChattingInput = null;
+
+    public override void Init()
+	{
+        
+    }
+
+	public override void Binding()
+	{
+		
+	}
+
+    public override void ShowCloseUI(bool IsShowClose)
+    {
+        
+    }
+}
