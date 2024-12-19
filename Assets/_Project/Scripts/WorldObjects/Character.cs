1046348dfab28c4f6a00118fafffeae3f58d5e47
LeeSangHwee <tkdgnl56@naver.com>
2024-12-19 20:34:05 +0900

    fix 보스 이동 속도 수정

--- Assets/_Project/Scripts/WorldObjects/Character.cs
+++ Assets/_Project/Scripts/WorldObjects/Character.cs
@@ -41,7 +41,7 @@ public class Character : FSMController<CharacterState, CharacterFSM, CharacterDa
     private void Awake()
     {
         if (UserInfo.myInfo.roleType == eRoleType.psychopass)
-            speed = 4.0f;
+            speed = 3.8f;
         else if (UserInfo.myInfo.roleType == eRoleType.bodyguard)
             speed = 3.0f;
         else
